6c79f97d3292d9b497660d6a8e89e5e682085848
coherent <mnb51817@gmail.com>
2023-03-09 20:26:44 +0800

    buffer overflow problem

--- app/src/main/java/com/example/wolo/MainActivity2.java
+++ app/src/main/java/com/example/wolo/MainActivity2.java
@@ -1,122 +1,122 @@
 package com.example.wolo;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-@SuppressLint("SetTextI18n")
 public class MainActivity2 extends AppCompatActivity {
-    Thread Thread1 = null;
+
+    private Handler mMainHandler;
+    private Socket socket;
+    private ExecutorService mThreadPool;
+    String response;
+
     EditText etIP, etPort;
     TextView tvMessages;
-    EditText etMessage;
-    Button btnSend;
     String SERVER_IP;
     int SERVER_PORT;
+
+    InputStream is;
+    InputStreamReader isr ;
+    BufferedReader br ;
+
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
         etIP = findViewById(R.id.etIP);
         etPort = findViewById(R.id.etPort);
         tvMessages = findViewById(R.id.tvMessages);
-        etMessage = findViewById(R.id.etMessage);
-        btnSend = findViewById(R.id.btnSend);
         Button btnConnect = findViewById(R.id.btnConnect);
-        btnConnect.setOnClickListener(new View.OnClickListener() {
+        mThreadPool = Executors.newCachedThreadPool();
+
+        mMainHandler = new Handler() {
             @Override
-            public void onClick(View v) {
+            public void handleMessage(Message msg) {
+                System.out.println(response);
+                tvMessages.setText(response);
+            }
+        };
+
+        btnConnect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
                 tvMessages.setText("");
                 SERVER_IP = etIP.getText().toString().trim();
                 SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
-                Thread1 = new Thread(new Thread1());
-                Thread1.start();
-            }
-        });
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = etMessage.getText().toString().trim();
-                if (!message.isEmpty()) {
-                    new Thread(new Thread3(message)).start();
-                }
-            }
-        });
-    }
-    private PrintWriter output;
-    private BufferedReader input;
-    class Thread1 implements Runnable {
-        public void run() {
-            Socket socket;
-            try {
-                socket = new Socket(SERVER_IP, SERVER_PORT);
-                output = new PrintWriter(socket.getOutputStream());
-                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-                runOnUiThread(new Runnable() {
+                mThreadPool.execute(new Runnable() {
                     @Override
                     public void run() {
-                        tvMessages.setText("Connected\n");
+
+                        try {
+
+                            // 创建Socket对象 & 指定服务端的IP 及 端口号
+                            socket = new Socket(SERVER_IP, SERVER_PORT);
+
+                            System.out.println(socket.isConnected());
+
+                            is = socket.getInputStream();
+                            isr = new InputStreamReader(is);
+                            br = new BufferedReader(isr);
+                            response = br.readLine();
+                            System.out.println("fsgfdg");
+                            System.out.println(response);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
                     }
                 });
-                new Thread(new Thread2()).start();
-            } catch (IOException e) {
-                e.printStackTrace();
+
             }
-        }
-    }
-    class Thread2 implements Runnable {
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    final String message = input.readLine();
-                    System.out.println(message);
-                    if (message != null) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                tvMessages.append("server: " + message + "\n");
-                            }
-                        });
-                    } else {
-                        Thread1 = new Thread(new Thread1());
-                        Thread1.start();
-                        return;
+        });
+
+        tvMessages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 利用线程池直接开启一个线程 & 执行该线程
+                mThreadPool.execute(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        try {
+                            is = socket.getInputStream();
+                            isr = new InputStreamReader(is);
+                            br = new BufferedReader(isr);
+                            response = br.readLine();
+                            Message msg = Message.obtain();
+                            msg.what = 0;
+                            mMainHandler.sendMessage(msg);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                });
+
             }
-        }
-    }
-    class Thread3 implements Runnable {
-        private String message;
-        Thread3(String message) {
-            this.message = message;
-        }
-        @Override
-        public void run() {
-            output.write(message);
-            output.flush();
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    tvMessages.append("client: " + message + "\n");
-                    etMessage.setText("");
-                }
-            });
-        }
+        });
+
     }
 }
-
-//reference:
-//https://www.tutorialspoint.com/sending-and-receiving-data-with-sockets-in-android
